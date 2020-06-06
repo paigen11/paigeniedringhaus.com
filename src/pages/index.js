c9f0e0b937199569c0390f247b38770d8ec4e302
Paige Niedringhaus <paigen11@gmail.com>
2020-06-06 18:18:08 -0400

    updating main pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,19 +3,32 @@ import Helmet from 'react-helmet';
 import config from '../../data/SiteConfig';
 import favicon from '../images/favicon-pn.png';
 import Layout from '../components/Layout/Layout';
+import paige from '../../content/images/paige-smile.jpg';
+import PostListing from '../components/PostListing/PostListing';
 import '../styles/pages.scss';
 
-const App = () => (
-  <Layout>
-    <div className="main-page page-body">
-      <Helmet>
-        <meta name="description" content={config.siteDescription} />
-        <link rel="shortcut icon" type="image/png" href={favicon} />
-        <html lang="en" />
-      </Helmet>
-      <h1>Main Page</h1>
-    </div>
-  </Layout>
-);
+const App = () => {
+  return (
+    <Layout>
+      <div className="main-page page-body">
+        <Helmet>
+          <meta name="description" content={config.siteDescription} />
+          <link rel="shortcut icon" type="image/png" href={favicon} />
+          <html lang="en" />
+        </Helmet>
+        <div className="main-intro">
+          <h1>Hi everyone, I'm Paige</h1>
+          <h2>
+            I'm a full stack software engineer with a focus on frontend
+            development. I write about web development, JavaScript and
+            technology.
+          </h2>
+        </div>
+        <img src={paige} alt="paige picture" />
+        <PostListing />
+      </div>
+    </Layout>
+  );
+};
 
 export default App;
--- src/pages/media.js
+++ src/pages/media.js
@@ -6,14 +6,18 @@ import speaking from '../../data/speaking';
 import publications from '../../data/publications';
 import podcasts from '../../data/podcasts';
 import Layout from '../components/Layout/Layout';
+import logRocket from '../images/thumbnails/logRocket.jpg';
+import butterCMS from '../images/thumbnails/butterCMS.png';
 import '../styles/pages.scss';
 
 const MediaPage = () => {
-  console.log(speaking);
-  console.log(podcasts);
-  console.log(publications);
-  console.log(Object.entries(publications));
+  const companyMap = {
+    ButterCMS: butterCMS,
+    LogRocket: logRocket,
+  };
+
   const companies = Object.entries(publications);
+
   return (
     <Layout>
       <div className="media-container page-body">
@@ -21,61 +25,99 @@ const MediaPage = () => {
         <h1>Media</h1>
         <p>
           In addition to my own <Link to="/blog">blog posts</Link>, I've been
-          fortunate enough to share what I've learned through talks at
-          conferences and meetups, in writing blogs for companies, and speaking
-          as a guest on podcasts.
+          fortunate enough to share what I've learned through speaking at
+          conferences and meetups, writing technical articles for companies, and
+          appearing as a guest on podcasts.
         </p>
-        <h3>Speaking engagements</h3>
+        <p>
+          Since 2020 I've also been a regular host on the popular tech podcast{' '}
+          <a
+            href="https://devchat.tv/react-round-up/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React Round Up
+          </a>{' '}
+          , a bi-monthly podcast dedicated to React and the development
+          community. Each episode we talk to different experts in the community
+          about all things React and beyond.
+        </p>
+        <h2>Talks</h2>
         <ul>
-          {speaking.map((talk) => {
+          {speaking.map((talk, i) => {
             return (
-              <li key={talk.title}>
-                <img className="media-images" src={talk.img} />
-                <a href={talk?.path ? talk.path : null} target="_blank">
-                  {talk.title}
-                </a>{' '}
-                - {talk.date}
-                {talk.slides && (
-                  <ul>
-                    <li>
-                      <a href={talk.slides} target="_blank">
-                        Slides
-                      </a>
-                    </li>
-                  </ul>
-                )}
-              </li>
+              <>
+                <li className="media-items" key={talk.title}>
+                  <img
+                    className="media-images"
+                    src={talk.img}
+                    alt="organization logo"
+                  />
+                  {talk.path ? (
+                    <a
+                      href={talk.path}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {talk.title}
+                    </a>
+                  ) : (
+                    <>{talk.title}</>
+                  )}
+                  &nbsp;- {talk.date}
+                </li>
+              </>
             );
           })}
         </ul>
-        <h3>Podcast episodes</h3>
+        <h2>Podcast interviews</h2>
         <ul>
           {podcasts.map((episode) => {
             return (
-              <li key={episode.title}>
-                <a href={episode.path} target="_blank">
+              <li className="media-items" key={episode.title}>
+                <img
+                  className="media-images"
+                  src={episode.img}
+                  alt="podcast logo"
+                />
+                <a
+                  href={episode.path}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   {episode.title}
-                </a>{' '}
-                - {episode.date}
+                </a>
+                &nbsp;- {episode.date}
               </li>
             );
           })}
         </ul>
-        <h3>Company blog posts</h3>
+        <h2>Company articles</h2>
         {companies.map((publication) => {
           const companyName = publication[0];
           const articles = publication[1];
           return (
             <article key={companyName}>
-              <h4>{companyName}</h4>
+              <span className="media-items companies">
+                <img
+                  src={companyMap[companyName]}
+                  className="media-images"
+                  alt="company logo"
+                />
+                <h3>{companyName}</h3>
+              </span>
               <ul>
                 {articles.map((article) => {
                   return (
-                    <li key={article.title}>
-                      <a href={article.path} target="_blank">
+                    <li className="media-items articles" key={article.title}>
+                      <a
+                        href={article.path}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
                         {article.title}
-                      </a>{' '}
-                      - {article.date}
+                      </a>
+                      &nbsp;- {article.date}
                     </li>
                   );
                 })}
