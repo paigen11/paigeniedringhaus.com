b5d20d627cb8871e4d48321a35969b842abf1d15
Paige Niedringhaus <paigen11@gmail.com>
2020-06-18 11:45:05 -0400

    adding more speaking engagements

--- data/speaking.js
+++ data/speaking.js
@@ -10,21 +10,18 @@ export default [
     date: 'February 23, 2017',
     path:
       'https://www.meetup.com/learn-to-code-in-atlanta-digitalcrafts/events/237763832',
-    slides: null,
     img: digitalCrafts,
   },
   {
     title: 'The Home Depot Tech Talk: Migrating from AngularJS to React',
     date: 'June 14, 2018',
     path: null,
-    slides: null,
     img: theHomeDepot,
   },
   {
     title: 'The Home Depot Tech Talk: Docker 102',
     date: 'June 27, 2018',
     path: null,
-    slides: null,
     img: theHomeDepot,
   },
   {
@@ -32,7 +29,6 @@ export default [
       'Girl Develop It Panelist Series: A Day in the Life of UX Designers and Developers',
     date: 'July 10, 2018',
     path: 'https://www.meetup.com/Girl-Develop-It-Atlanta/events/251904253',
-    slides: null,
     img: girlDevelopIt,
   },
   {
@@ -40,29 +36,32 @@ export default [
       'The Home Depot Tech Talk: Google Jib: Getting Expert Docker Results Without Any Knowledge of Docker',
     date: 'October 3, 2018',
     path: null,
-    slides: null,
+    img: theHomeDepot,
+  },
+  {
+    title: 'The Home Depot Tech Talk: Responsive Design with ReactJS',
+    date: 'October 14, 2019',
+    path: null,
     img: theHomeDepot,
   },
   {
     title: 'Connect.Tech 2019: Responsive Design with ReactJS',
     date: 'October 18, 2019',
     path: 'https://connect.tech/session/?id=4664',
-    slides: 'https://bit.ly/ct-responsive-web-design',
     img: connectTech,
   },
   {
     title: 'Virtual Meetup: React Testing Library Tips & Tricks',
     date: 'April 30, 2020',
     path: 'https://www.crowdcast.io/e/react-virtual-meetup',
-    slides: 'https://bit.ly/meetup-rtl-tips',
     img: react,
   },
   {
     title:
       'DigitalCrafts Elective Workshop: React Testing Library Tips and Tricks',
     date: 'June 3, 2020',
-    path: null,
-    slides: 'https://bit.ly/meetup-rtl-tips',
+    path:
+      'https://us02web.zoom.us/rec/share/-e1HIK7L8H5OZKvXxk3dda8ZQ6Tqeaa8gSEW-vFezSQQqxeDKTctYEhQODwaXE8?startTime=1591223149000',
     img: digitalCrafts,
   },
 ];
