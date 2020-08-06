3a6cb682d103de8f14f25c1692312807bd6cfbff
Paige Niedringhaus <paigen11@gmail.com>
2020-08-06 17:42:29 -0400

    updates to post listing

--- src/components/PostListing/PostListing.js
+++ src/components/PostListing/PostListing.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'gatsby';
 import Img from 'gatsby-image';
 import { usePostListingQuery } from '../../Hooks/usePostListingQuery';
@@ -8,9 +8,16 @@ import PostTags from '../PostTags/PostTags';
 import './PostListing.scss';
 
 const PostListing = ({ partial = false }) => {
-  const getPostList = () => {
+  const [fullPostList, setFullPostList] = useState([]);
+  const emptyQuery = '';
+  const [state, setState] = useState({
+    filteredPostList: [],
+    query: emptyQuery,
+  });
+  const localSitePosts = usePostListingQuery();
+
+  const getAndFormatAllPosts = (posts) => {
     const postList = [];
-    const posts = usePostListingQuery();
     posts.allMarkdownRemark.edges.forEach((postEdge) => {
       postList.push({
         path: postEdge.node.fields.slug,
@@ -22,49 +29,95 @@ const PostListing = ({ partial = false }) => {
         timeToRead: postEdge.node.timeToRead,
       });
     });
-    const fullList = postList.concat(mediumBlogs);
-    const sortedPostsList = sortArrayByDate(fullList);
-    console.log(sortedPostsList);
-    const uniqueTags = new Set();
+    const fullPostList = postList.concat(mediumBlogs);
+    const sortedPostsList = sortArrayByDate(fullPostList);
     sortedPostsList.forEach((post) => {
-      console.log(post.tags);
-      post.tags?.forEach((tag) => {
-        uniqueTags.add(tag);
-      });
+      post.tags?.sort();
     });
-    console.log(uniqueTags);
+    // todo remove this and replace with diff component on home page
     if (partial) {
+      setFullPostList(sortedPostsList);
       return sortedPostsList.slice(0, 4);
     }
+    setFullPostList(sortedPostsList);
     return sortedPostsList;
   };
 
-  const postList = getPostList();
+  useEffect(() => {
+    getAndFormatAllPosts(localSitePosts);
+  }, []);
+
+  const filterPosts = (e) => {
+    const query = e.target.value;
+    const filteredPostList = fullPostList.filter((post) => {
+      return (
+        post.title.toLowerCase().includes(query.toLowerCase()) ||
+        (post.subTitle &&
+          post.subTitle.toLowerCase().includes(query.toLowerCase())) ||
+        (post.excerpt &&
+          post.excerpt.toLowerCase().includes(query.toLowerCase())) ||
+        (post.tags &&
+          post.tags.join('').toLowerCase().includes(query.toLowerCase()))
+      );
+    });
+
+    setState({
+      query,
+      filteredPostList,
+    });
+  };
+
+  const { filteredPostList, query } = state;
+  const hasSearchResults = filteredPostList && query !== emptyQuery;
+  const posts = hasSearchResults ? filteredPostList : fullPostList;
 
   return (
-    <div className="posts-wrapper">
-      {postList.map((post) => (
-        <div className="post">
-          <p className="post-date">
-            {post.date} {'\u2022'} {post.timeToRead} min read
-          </p>
-          {post.path ? (
-            <Link to={post.path} key={post.title}>
-              <p className="post-title">{post.title}</p>
-              <Img fixed={post.thumbnail} />
-              {post.excerpt && <p>{post.excerpt}</p>}
-            </Link>
-          ) : (
-            <a href={post.url} target="_blank" rel="noopener noreferrer">
-              <p className="post-title">{post.title}</p>
-              <img src={post.img} style={{ width: 200 }} />
-              {post.subTitle ? <p>{post.subTitle}</p> : <p></p>}
-            </a>
-          )}
-          <PostTags tags={post.tags} />
+    <>
+      {!posts.length && query === emptyQuery && (
+        <div className="loader-wrapper">
+          <div className="loader"></div>
+          <h2>Loading blog posts</h2>
         </div>
-      ))}
-    </div>
+      )}
+      <>
+        <input
+          className="searchInput"
+          type="text"
+          aria-label="Search"
+          placeholder="Filter blog posts by title or tag"
+          onChange={(e) => filterPosts(e)}
+        ></input>
+        <div className="posts-wrapper">
+          {posts.length ? (
+            posts.map((post) => (
+              <div className="post">
+                <p className="post-date">
+                  {post.date} {'\u2022'} {post.timeToRead} min read
+                </p>
+                {post.path ? (
+                  <Link to={post.path} key={post.title}>
+                    <p className="post-title">{post.title}</p>
+                    <Img fixed={post.thumbnail} />
+                    {post.excerpt && <p>{post.excerpt}</p>}
+                  </Link>
+                ) : (
+                  <a href={post.url} target="_blank" rel="noopener noreferrer">
+                    <p className="post-title">{post.title}</p>
+                    <img src={post.img} style={{ width: 200 }} />
+                    {post.subTitle ? <p>{post.subTitle}</p> : <p></p>}
+                  </a>
+                )}
+                <PostTags tags={post.tags} />
+              </div>
+            ))
+          ) : (
+            <div class="empty-results">
+              <h2>Sorry, no search results match your query.</h2>
+            </div>
+          )}
+        </div>
+      </>
+    </>
   );
 };
 
