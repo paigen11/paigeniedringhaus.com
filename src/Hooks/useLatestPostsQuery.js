9b6a8a143a1169b8b586892e5e32e08669253d20
Paige Niedringhaus <paigen11@gmail.com>
2020-08-11 18:34:40 -0400

    adding another hook

---
+++ src/Hooks/useLatestPostsQuery.js
@@ -0,0 +1,29 @@
+import { useStaticQuery, graphql } from 'gatsby';
+
+export const useLatestPostsQuery = () => {
+  const latestPostsData = useStaticQuery(graphql`
+    query LatestPostQuery {
+      allMarkdownRemark(
+        sort: { fields: [fields___date], order: DESC }
+        limit: 4
+      ) {
+        edges {
+          node {
+            fields {
+              slug
+              date(formatString: "MMM D, YYYY")
+            }
+            excerpt
+            timeToRead
+            frontmatter {
+              title
+              tags
+              date
+            }
+          }
+        }
+      }
+    }
+  `);
+  return latestPostsData;
+};
