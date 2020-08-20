b07a0480ec3fa7c873f5a185d228511538a2b4d0
Paige Niedringhaus <paigen11@gmail.com>
2020-08-20 17:40:33 -0400

    updating templates

--- src/templates/post.js
+++ src/templates/post.js
@@ -3,48 +3,44 @@ import Helmet from 'react-helmet';
 import { graphql } from 'gatsby';
 import Layout from '../components/Layout/Layout';
 import UserInfo from '../components/UserInfo/UserInfo';
-import Disqus from '../components/Disqus/Disqus';
 import PostTags from '../components/PostTags/PostTags';
-import SocialLinks from '../components/SocialLinks/SocialLinks';
 import SEO from '../components/SEO/SEO';
-import Footer from '../components/Footer/Footer';
+import SocialLinks from '../components/SocialLinks/SocialLinks';
 import config from '../../data/SiteConfig';
 import './b16-tomorrow-dark.css';
 import './post.scss';
 
-export default class PostTemplate extends React.Component {
-  render() {
-    const { data, pageContext } = this.props;
-    const { slug } = pageContext;
-    const postNode = data.markdownRemark;
-    const post = postNode.frontmatter;
-    if (!post.id) {
-      post.id = slug;
-    }
-
-    return (
-      <Layout>
-        <div>
-          <Helmet>
-            <title>{`${post.title} | ${config.siteTitle}`}</title>
-          </Helmet>
-          {/* <SEO postPath={slug} postNode={postNode} postSEO /> */}
-          <div className="post-wrapper">
-            <h1>{post.title}</h1>
-            <div dangerouslySetInnerHTML={{ __html: postNode.html }} />
-            <div className="post-meta">
-              <PostTags tags={post.tags} />
-              <SocialLinks postPath={slug} postNode={postNode} />
-            </div>
-            <UserInfo config={config} />
-            {/* <Disqus postNode={postNode} /> */}
-            <Footer config={config} />
-          </div>
-        </div>
-      </Layout>
-    );
+const Post = (props) => {
+  const { data, pageContext } = props;
+  const { slug } = pageContext;
+  const postNode = data.markdownRemark;
+  const post = postNode.frontmatter;
+  if (!post.id) {
+    post.id = slug;
   }
-}
+
+  return (
+    <Layout>
+      <div>
+        <Helmet>
+          <title>{`${post.title} | ${config.siteTitle}`}</title>
+        </Helmet>
+        {/* <SEO postPath={slug} postNode={postNode} postSEO /> */}
+        <div className="post-wrapper">
+          <h1>{post.title}</h1>
+          <div dangerouslySetInnerHTML={{ __html: postNode.html }} />
+          <div className="post-meta">
+            <PostTags tags={post.tags} />
+            <SocialLinks postPath={slug} postNode={postNode} />
+          </div>
+          <UserInfo config={config} />
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Post;
 
 /* eslint no-undef: "off" */
 export const pageQuery = graphql`
