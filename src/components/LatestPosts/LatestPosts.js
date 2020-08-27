a2004f9306e5e9320ec6cd8ef9b12dc9f76381c8
Paige Niedringhaus <paigen11@gmail.com>
2020-08-27 17:35:56 -0400

    adding new component

---
+++ src/components/LatestPosts/LatestPosts.js
@@ -0,0 +1,96 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'gatsby';
+import { useLatestPostsQuery } from '../../Hooks/useLatestPostsQuery';
+import Loader from '../Loader/Loader';
+import PostTags from '../PostTags/PostTags';
+import { sortArrayByDate } from '../../helpers/helpers';
+import mediumBlogs from '../../../data/mediumBlogs';
+import './LatestPosts.scss';
+
+const LatestPosts = () => {
+  const [latestPostList, setLatestPostList] = useState([]);
+  const localSitePosts = useLatestPostsQuery();
+
+  const getAndFormatLatestPosts = (posts) => {
+    const latestMediumBlogs = mediumBlogs.slice(
+      mediumBlogs.length - 4,
+      mediumBlogs.length,
+    );
+    const latestPostList = [];
+    posts.allMarkdownRemark.edges.forEach((postEdge) => {
+      latestPostList.push({
+        path: postEdge.node.fields.slug,
+        title: postEdge.node.frontmatter.title,
+        date: postEdge.node.fields.date,
+        excerpt: postEdge.node.excerpt,
+      });
+    });
+    const fullPostList = latestPostList.concat(latestMediumBlogs);
+    const sortedPostsList = sortArrayByDate(fullPostList);
+    sortedPostsList.forEach((post) => {
+      post.tags?.sort();
+    });
+    const latestPosts = sortedPostsList.slice(0, 4);
+
+    setLatestPostList(latestPosts);
+    return latestPosts;
+  };
+
+  useEffect(() => {
+    getAndFormatLatestPosts(localSitePosts);
+  }, []);
+
+  const posts = latestPostList;
+
+  return (
+    <>
+      {!posts.length && <Loader message="Loading lastest blog posts" />}
+      <>
+        <div className="latest-posts-wrapper">
+          {posts.length
+            ? posts.map((post, index) => (
+                <div className="post" key={index}>
+                  {post.path ? (
+                    <>
+                      <Link to={post.path} key={post.title}>
+                        <p className="post-title">{post.title}</p>
+                      </Link>
+                      {post.excerpt && <p>{post.excerpt}</p>}
+                      <PostTags tags={post.tags} />
+                      <Link to={post.path}>
+                        <p className="read-more">Read full article &gt;</p>
+                      </Link>
+                    </>
+                  ) : (
+                    <>
+                      <a
+                        href={post.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <p className="post-title">{post.title}</p>
+                      </a>
+                      <p>{post.subTitle}</p>
+                      <PostTags tags={post.tags} />
+                      <a
+                        href={post.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <p className="read-more">Read full article &gt;</p>
+                      </a>
+                    </>
+                  )}
+                </div>
+              ))
+            : null}
+          <p>
+            <Link to="/blog">View all articles</Link>
+          </p>
+        </div>
+      </>
+    </>
+  );
+};
+
+export default LatestPosts;
