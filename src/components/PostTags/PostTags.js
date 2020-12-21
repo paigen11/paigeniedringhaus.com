84c1d6e7b2a4a72124fe371fca1196e841d7716e
Paige Niedringhaus <paigen11@gmail.com>
2020-12-21 17:42:28 -0500

    updating post tags

--- src/components/PostTags/PostTags.js
+++ src/components/PostTags/PostTags.js
@@ -3,14 +3,20 @@ import { kebabCase } from 'lodash';
 import './PostTags.scss';
 
 const PostTags = ({ tags }) => {
+  const tagList = tags?.map((tag) => (
+    <div key={tag} className={kebabCase(tag)}>
+      {tag}
+    </div>
+  ));
+
   return (
     <div className="post-tag-container">
-      {tags &&
-        tags.map((tag) => (
-          <div key={tag} className={kebabCase(tag)}>
-            {tag}
-          </div>
-        ))}
+      {tags ? (
+        <>
+          <span>Tags: </span>
+          {tagList}
+        </>
+      ) : null}
     </div>
   );
 };
