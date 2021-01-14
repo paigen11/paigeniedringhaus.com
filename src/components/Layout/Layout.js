065141f3350baacce085665ecdb7f4b6cc82682a
Paige Niedringhaus <paigen11@gmail.com>
2021-01-14 16:50:26 -0500

    layout updates for responsive

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,23 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Helmet from 'react-helmet';
+import classNames from 'classnames';
 import config from '../../../data/SiteConfig';
 import favicon from '../../images/favicon-pn.png';
 import Navigation from '../Navigation/Navigation';
 import Footer from '../Footer/Footer';
+import MobileNavContext from '../../context/MobileNavContext';
 import './Layout.scss';
 
 const MainLayout = ({ children }) => {
+  const [mobileNavOpen, setMobileNavOpen] = useState(false);
+  const [showMobileNavMenu, setShowMobileNavMenu] = useState(false);
+  const layoutContainerPositioning = classNames('layout-container', {
+    open: mobileNavOpen,
+  });
+
+  useEffect(() => {
+    const checkInitialViewportSize = () => {
+      if (window.innerWidth > 767) {
+        setShowMobileNavMenu(false);
+      } else if (window.innerWidth < 767) {
+        setShowMobileNavMenu(true);
+      }
+    };
+
+    checkInitialViewportSize();
+  }, []);
+
   return (
-    <div className="layout-container">
-      <Helmet>
-        <meta name="description" content={config.siteDescription} />
-        <link rel="shortcut icon" type="image/png" href={favicon} />
-        <html lang="en" />
-      </Helmet>
-      <Navigation />
-      {children}
-      <Footer />
-    </div>
+    <MobileNavContext.Provider
+      value={{
+        mobileNavOpen,
+        showMobileNavMenu,
+        toggleMobileNav: (navState) => setMobileNavOpen(navState),
+        toggleMobileNavView: (showNavBool) => setShowMobileNavMenu(showNavBool),
+      }}
+    >
+      <div className={layoutContainerPositioning}>
+        <Helmet>
+          <meta name="description" content={config.siteDescription} />
+          <link rel="shortcut icon" type="image/png" href={favicon} />
+          <html lang="en" />
+        </Helmet>
+        <Navigation />
+        {children}
+        <Footer />
+      </div>
+    </MobileNavContext.Provider>
   );
 };
 
