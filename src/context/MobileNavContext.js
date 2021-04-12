25c5ebfd7f353771b8121c98962349e8d5c5b9c6
Paige Niedringhaus <paigen11@gmail.com>
2021-04-12 14:28:22 -0400

    code cleanup

--- src/context/MobileNavContext.js
+++ src/context/MobileNavContext.js
@@ -3,6 +3,8 @@ import { createContext } from 'react';
 const MobileNavContext = createContext({
   mobileNavOpen: false,
   toggleMobileNav: () => {},
+  showMobileNavMenu: false,
+  toggleMobileNavView: () => {},
 });
 
 export default MobileNavContext;
